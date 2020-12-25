69569179a85cba23c6c1ed0af3d1348331b69e01
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-12-24 20:41:54 -0600

    Add endpoints task actions

--- app.js
+++ app.js
@@ -41,10 +41,72 @@ app.post('/lists', (req, res) => {
 
 app.patch('/lists/:id', (req, res) => {
 
+    List.findOneAndUpdate({ _id: req.params.id }, {
+        $set: req.body
+    }).then(() => {
+        res.sendStatus(200);
+    });
 });
 
 app.delete('/lists/:id', (req, res) => {
 
+
+    List.findOneAndRemove({
+        _id: req.params.id
+    }).then((removedListDoc) => {
+        res.send(removedListDoc);
+    })
+
+})
+
+app.get('/lists/:listId/tasks', (req, res) => {
+    Task.find({
+        _listId: req.params.listId
+    }).then((tasks) => {
+        res.send(tasks);
+    })
+})
+
+app.post('/lists/:listId/tasks', (req, res) => {
+    let newTask = new Task({
+        title: req.body.title,
+        _listId: req.params.listId
+    });
+
+    newTask.save().then((newTaskDoc) => {
+        res.send(newTaskDoc);
+    })
+})
+
+app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndUpdate({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }, {
+        $set: req.body
+    }
+    ).then(() => {
+        res.sendStatus(200);
+    })
+})
+
+app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndRemove({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }
+    ).then((removedTaskDoc) => {
+        res.send(removedTaskDoc);
+    })
+})
+
+app.get('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOne({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }).then((task) => {
+        res.send(task);
+    })
 })
 
 app.listen(3000, () => {
